f046c82aa4f032041437cc3a7e446a4191bc567d
Maxime Gillot <max.gillot.69@gmail.com>
2025-09-22 21:39:10 +0200

    Update MobTest.cs

--- Demo/MobTest.cs
+++ Demo/MobTest.cs
@@ -2,34 +2,30 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.AI;
 
-namespace StarterAssets
+public class MobTest : MonoBehaviour
 {
-    public class MobTest : MonoBehaviour
+    private Camera cam;
+    private NavMeshAgent agent;
+
+    private void Awake()
     {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
 
-        private Camera cam;
-        private NavMeshAgent agent;
-
-        private void Awake()
+    private void Update()
+    {
+        if (InputGlobalManager.Instance != null)
         {
-            cam = Camera.main;
-            agent = GetComponent<NavMeshAgent>();
-        }
-
-        private void Update()
-        {
-            if (InputGlobalManager.Instance != null)
+            if (InputGlobalManager.Instance.GetAttackInputDown())
             {
-                if (InputGlobalManager.Instance.GetAttackInputDown())
+
+                Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
                 {
-
-                    Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
-                    RaycastHit hit;
-
-                    if (Physics.Raycast(ray, out hit))
-                    {
-                        agent.SetDestination(hit.point);
-                    }
+                    agent.SetDestination(hit.point);
                 }
             }
         }
