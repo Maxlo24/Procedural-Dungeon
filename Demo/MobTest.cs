ecdc20a702dd37556ef0d7697408b41a28206329
Maxime Gillot <max.gillot.69@gmail.com>
2025-09-22 20:47:27 +0200

    Moved demo assets

---
+++ Demo/MobTest.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.AI;
+
+namespace StarterAssets
+{
+    public class MobTest : MonoBehaviour
+    {
+
+        private Camera cam;
+        private NavMeshAgent agent;
+
+        private void Awake()
+        {
+            cam = Camera.main;
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            if (InputGlobalManager.Instance != null)
+            {
+                if (InputGlobalManager.Instance.GetAttackInputDown())
+                {
+
+                    Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+                    RaycastHit hit;
+
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        agent.SetDestination(hit.point);
+                    }
+                }
+            }
+        }
+    }
+}
